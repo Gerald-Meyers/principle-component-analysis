061818d4f8415bfaaff5814a3ba4926c1bb16086
Gerald <Geraldsmeyers@gmail.com>
2025-11-06 19:17:19 +0000

    Added some additional information, and improved the information in the README. More work to be done.

--- PrincipleComponentAnalysis.py
+++ PrincipleComponentAnalysis.py
@@ -1,8 +1,19 @@
 from typing import Any, Callable, Iterable, Optional, Union
 
-from numpy import (array, asarray, average,  # float80,; float96,; float256,
-                   float16, float32, float64, float128, int_, mean, ndarray,
-                   random, std)
+from numpy import (  # float80,; float96,; float256,
+    array,
+    asarray,
+    average,
+    float16,
+    float32,
+    float64,
+    float128,
+    int_,
+    mean,
+    ndarray,
+    random,
+    std,
+)
 from numpy.linalg import eig
 from numpy.typing import NDArray
 
@@ -10,11 +21,26 @@ PythonScalars = (int, float)
 PythonScalar: type = Union[*PythonScalars]
 PythonArray = Iterable[PythonScalar]
 
+FloatScalars = (
+    int_,
+    float16,
+    float32,
+    float64,
+    # float80,
+    # float96,
+    # float128,
+    # float256
+)
+FloatScalar = Union[*FloatScalars]
+FloatArray = NDArray[FloatScalar]
+CallableScalar = Callable[[FloatArray], FloatScalar]
+
 
 class PCA:
 
     def __init__(self, dataset, n_components):
         self.dataset = dataset
+        self.n_samples, self.n_features = dataset.shape
         self.n_components = n_components
         self.component_vectors = array([])
         self.explained_variance = array([])
@@ -41,11 +67,20 @@ class PCA:
         self.explained_variance = eigenvalues / total_variance
 
     def transform(self):
-        ...
+        # 1. Normalize the original dataset
+        normalized_dataset = self._normalize()
+
+        # 2. Get the top n_components
+        top_n_components = self.component_vectors[0: self.n_components]
+
+        # 3. Project the data onto the principal components
+        self.transformed_dataset = normalized_dataset @ top_n_components.T
+        return self.transformed_dataset
 
     def fit_transform(self):
-        ...
-        return self.transformed_dataset
+        self.fit()
+        self.transform()
+        return self.get_transformed_dataset()
 
     def get_component_vectors(self):
         return self.component_vectors
@@ -66,17 +101,17 @@ class PCA:
         mean_vector = mean(self.dataset, axis=0)
         std_vector = std(self.dataset, axis=0)
 
-        return (self.dataset - mean_vector) / std_vector
+        self.normalized_dataset = (self.dataset - mean_vector) / std_vector
 
     def _covariance_matrix(self):
-        normalized_dataset = self._normalize()
-        n_samples = normalized_dataset.shape[0]
-        return normalized_dataset.T @ normalized_dataset / (n_samples - 1)
+        self.covariance_matrix = \
+            self.normalized_dataset.T @ self.normalized_dataset / \
+            (self.n_samples - 1)
 
     def save_to_file(self, filename):
         with open(filename, 'w') as f:
-            f.write(dataset)
-            f.write(component_vectors)
-            f.write(explained_variance)
-            f.write(transformed_dataset)
-            f.write(n_components)
+            f.write(str(self.dataset))
+            f.write(str(self.component_vectors))
+            f.write(str(self.explained_variance))
+            f.write(str(self.transformed_dataset))
+            f.write(str(self.n_components))
